ebe2fa97ac3ef952c603a00a371c46f7f2134157
Mary Martinez <mary.diana.martinez@gmail.com>
2022-04-06 09:27:16 -0700

    problem 1 solution is working

--- src/components/Shape/Shape.js
+++ src/components/Shape/Shape.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Shape.css';
 
 export default function Shape(props) {
+  const handleShapeClick = (color) => {
+    props.setLargeCircleColor(color);
+    // console.log('clicking');
+  };
   const classes = `shape ${props.type} ${props.color} ${props.small ? 'small' : ''}`;
-  return <div className={classes}></div>;
+  return <div className={classes} onClick={() => handleShapeClick(props.color)}></div>;
 }
--- src/views/Problem1.js
+++ src/views/Problem1.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Shape from '../components/Shape/Shape';
 
 import './Problem1.css';
 
 export default function Problem1() {
+  const [largeCircleColor, setLargeCircleColor] = useState('red');
+
+  // const handleShapeClick = (selectedColor) => {
+  //   setLargeCircleColor(selectedColor);
+  // };
+
   return (
     <div className="problem-container">
       <h2>Problem 1: Props and State</h2>
@@ -13,12 +19,12 @@ export default function Problem1() {
         state, event listeners and update props to make that happen.
       </div>
       <div className="shape-controls">
-        <Shape color="blue" type="circle" small />
-        <Shape color="purple" type="circle" small />
-        <Shape color="green" type="circle" small />
+        <Shape color="blue" type="circle" small setLargeCircleColor={setLargeCircleColor}/>
+        <Shape color="purple" type="circle" small setLargeCircleColor={setLargeCircleColor}/>
+        <Shape color="green" type="circle" small setLargeCircleColor={setLargeCircleColor}/>
       </div>
       <div className="selected-shape">
-        <Shape color="red" type="circle"></Shape>
+        <Shape color={largeCircleColor} type="circle" setLargeCircleColor={setLargeCircleColor}></Shape>
       </div>
     </div>
   );
